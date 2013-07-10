4748d74955c5e6a9ab839bb5871f584a1fd66edb
tatuquitola@gmail.com <ubuntu@ip-172-31-26-109.us-west-2.compute.internal>
2013-07-10 21:34:03 +0000

    Added readfile

--- web.js
+++ web.js
@@ -2,8 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var openFile = function(whatFile){
+/*This function save to a file(whatFile) some text(whatText)*/
+	var fsObj = require('fs');
+	return fsObj.readFileSync(whatFile);
+	fsObj = null;
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(openFile('index.html'));
 });
 
 var port = process.env.PORT || 5000;
